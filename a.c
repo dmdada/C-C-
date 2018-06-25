929434ee1bdbbd93cd37aba7a8759209b72fe5f8
dmdada <xxx@gmail.com>
2018-06-25 17:11:07 +0800

    找到第一个字符“

---
+++ a.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void fun(char* str)
+{
+char tmp='a';
+char*src=str+1;
+int count =0;
+while(*str!='\0')
+{  count=0;
+  src=str+1;
+  while(*src!='\0')
+{
+if (*str==*src) 
+{
+  count++;
+  tmp=*src;
+}
+src++;
+}
+if (count == 1)
+{
+   printf("找到了字符是：%c\n", tmp);
+  return;         
+}
+str++;
+
+}
+printf("没找到！");
+}
+
+
+int main()
+{
+char*str="abacdebaf";
+fun(str);
+  return 0;
+}
