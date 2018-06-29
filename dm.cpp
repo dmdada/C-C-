522846e9340827c49ad0e62a0108fe27fce568fa
dmdada <xxx@gmail.com>
2018-06-29 22:43:04 +0800

    class test

--- dm.cpp
+++ dm.cpp
@@ -1,11 +1,59 @@
 #include<iostream>
 using namespace std;
-int main() {
- int arr[10];
- for (int i = 0; i < 10; i++)
- {
-   arr[i]=i;
-   cout<<arr[i]<<endl;
- }
+
+class mystring
+{
+  private:
+    int x;
+    int y;
+  public:
+    mystring(int a,int b)
+    {
+      x=a;
+      y=b;
+    }
+    mystring(const mystring &c)
+    {
+      x=c.x;
+      y=c.y;
+    }
+    mystring& operator =(const mystring &c)
+    {
+       if (this==&c)
+       {
+       return *this;  
+       }
+       x=c.x;
+       y=c.y;
+       return *this;
+    }
+    int sum();
+    int add();
+    ~mystring()
+    {
+
+      cout<<"delelet class!"<<endl;
+    }
+
+};
+int mystring::sum()
+{
+  return x-y;
+}
+int mystring::add()
+{
+return x+y;
+}
+ 
+int main() 
+{
+mystring A(3,1);
+mystring B=A;
+mystring C(10,20);
+mystring* ptr=&C;
+cout<<A.add()<<endl;
+cout<<B.sum()<<endl;
+cout << ptr->add() <<endl;
+cout << ptr->sum() <<endl;
   return 0;
 }
